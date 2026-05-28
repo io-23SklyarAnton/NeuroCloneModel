3f7648f531dff52bf4ac7ef05723fa5127fc8f0c
Anton <80kap.i.toshka@gmail.com>
2026-05-28 21:05:39 +0200

    removed bot.is_neuroclone_ready from receive_chat_message.py

--- src/bot_operations/application/features/receive_chat_message.py
+++ src/bot_operations/application/features/receive_chat_message.py
@@ -59,7 +59,7 @@ class CommandHandler:
         live_chat.append_message(incoming_message)
         self._uow.live_chat.update(live_chat)
 
-        if not bot.is_neuroclone_ready or bot.neuroclone_id is None:
+        if bot.neuroclone_id is None:
             await self._uow.commit()
             return Response(reply_text=None)
 
